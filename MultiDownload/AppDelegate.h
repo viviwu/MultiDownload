31899eaf866321f1246bf2b7c33b9c225dd2e92d
xcodevivi <vivi705wu@sina.com>
2015-07-27 15:59:44 +0800

    Initial Commit

---
+++ MultiDownload/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MultiDownload
+//
+//  Created by qway on 15/7/27.
+//  Copyright (c) 2015年 XW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ MultiDownload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiDownload
+//
+//  Created by qway on 15/7/27.
+//  Copyright (c) 2015年 XW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
